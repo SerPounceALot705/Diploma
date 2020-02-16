2ef181e4cf40046a49cd19b97a9e8d3832e67d6d
Valerie Dmitryuk <cosmic.pony705@gmail.com>
2020-02-16 19:41:02 +0800

    Создан класс GlideInit
    Функционал слайдера со страницы "О проекте" в секции "История коммитов" вынесен в отдельный класс

---
+++ src/js/utils/GlideInit.js
@@ -0,0 +1,26 @@
+import Glide from '@glidejs/glide'
+
+export default class GlideInit {
+    constructor() {
+        new Glide('.glide', {
+            type: 'carousel',
+            focusAt: 'center',
+            perView: 4,
+            breakpoints: {
+                1280: {
+                    perView : 3
+                },
+                1024 : {
+                    perView : 2
+                },
+                767: 
+                {
+                    perView: 1
+                },
+                425: {
+                    perView: 1
+                }
+            }
+        }).mount()    
+    }
+}
