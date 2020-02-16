6a924007ed09f8b06911ee555edd35a2eee26765
Valerie Dmitryuk <cosmic.pony705@gmail.com>
2020-02-16 20:04:22 +0800

    Добавлен вызов основного функционала для страницы "Аналитика"

--- src/js/analytics.js
+++ src/js/analytics.js
@@ -1,7 +1,17 @@
 import '../css/analytics.css';
+import DrawAnalytics from './utils/DrawAnalytics';
+import AgrigateAnalytics from './components/AgrigateAnalytics';
+import LocalStorageApi from './modules/LocalStorageApi';
 
-export class Analytics{
-    constructor() {
-        console.log("analytics");
-    }
-}
+const drawAnalytics = new DrawAnalytics();
+const agrigateAnalytics = new AgrigateAnalytics();
+const localStorage = new LocalStorageApi();
+
+const totalResults = agrigateAnalytics.getTotalResults();
+const agrigate = agrigateAnalytics.getAgrigateData();
+
+drawAnalytics.drawText(document.querySelector('#total-results'), totalResults);
+drawAnalytics.drawText(document.querySelector('#total-mention'), agrigate.totalTitle);
+drawAnalytics.drawText(document.querySelector('.statistics__title'), `Вы спросили: «${localStorage.getData("requestText")}»`)
+drawAnalytics.drawText(document.querySelector('.analytics-by-day__date'), new Date().toLocaleString('ru', {month : 'long'}))
+drawAnalytics.drawProcentWeek(agrigate);
