b41f047d2aa35c3e702c06bc92979ca0e3c22ff0
Valerie Dmitryuk <cosmic.pony705@gmail.com>
2020-02-16 20:02:51 +0800

    Добавлен вызов основного функционала страницы "Главная"

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,17 @@
 import '../css/index.css';
 
-class Main {
-    constructor(){
-        console.log('index')
-    }
-}
+import SearchInput from './components/SearchInput';
+import Validate from './utils/Validate';
+import DrawContainers from './utils/DrawContainers';
 
+const input = document.forms.search.elements.text;
+const validity = new Validate(input);
+
+input.addEventListener('focus', () => validity.isValid());
+input.addEventListener('input', () => validity.isValid());
+
+const draw = new DrawContainers(document.querySelector('.search-box'));
+draw.applyIndex();
+
+document.forms.search.addEventListener('submit', event => new SearchInput().getNews(event));   
 
