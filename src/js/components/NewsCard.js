4ae014989f3a18b437ccf1ff7c43bd0ea85ccd9d
Valerie Dmitryuk <cosmic.pony705@gmail.com>
2020-02-16 19:23:15 +0800

    Создан класс NewsCard
    Класс для создания разметки карточки новости

---
+++ src/js/components/NewsCard.js
@@ -0,0 +1,49 @@
+import Utils from '../utils/Utils';
+
+export default class NewsCard {
+    constructor () {}
+
+    create(data) {
+        const utils =  new Utils();
+        const newsCard = document.createElement("div");
+        newsCard.classList.add("card");
+        
+        const template = `
+            <img class=card-image src="" alt="фото новости">
+            <div class="card-info">
+                <div class="data"></div>
+                <h2 class="card-title"></h2>
+                <p class="card-text">}</p>
+                <a href="" class="card-link" target="_blank"></a>
+            </div>`;
+
+        
+        newsCard.insertAdjacentHTML('beforeend', template.trim());
+
+        let publishedAt = new Date(data.publishedAt)
+            .toLocaleDateString('ru-RU', 
+            {
+                year: 'numeric', 
+                month: 'long', 
+                day: 'numeric'
+            });
+            
+        publishedAt = utils.sliceString(publishedAt);
+        publishedAt = utils.insertSubString(publishedAt.length-5, publishedAt, ', ');
+
+        newsCard.querySelector('.card-image').src = (data.urlToImage == null) || (data.urlToImage == "") 
+                                                    ? require('../../images/not-img.png').default 
+                                                    : data.urlToImage;
+
+        newsCard.querySelector('.card-title').textContent = (window.innerWidth > 768) 
+                                                            ? (data.title) 
+                                                            : (data.title);
+
+        newsCard.querySelector('.card-text').textContent = (data.description);
+        newsCard.querySelector('.card-link').textContent = data.source.name;
+        newsCard.querySelector('.card-link').href = data.url;
+        newsCard.querySelector('.data').textContent = publishedAt;
+
+        return newsCard;
+    }
+}
