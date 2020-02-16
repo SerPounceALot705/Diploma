2b9f424d0539d403d584eb1ebdba96bda962b7c6
Valerie Dmitryuk <cosmic.pony705@gmail.com>
2020-02-16 19:47:41 +0800

    Создан класс Constants
    Класс, содержащий константы, используемые в других классах

---
+++ src/js/constants/Constants.js
@@ -0,0 +1,3 @@
+export const API_KEY = '7798f9c937f249c594a87e4725d25525';
+export const NEWS_URL = 'https://newsapi.org/v2/everything';
+export const GITHUB_URL = 'https://api.github.com/repos/serpouncealot705/diploma/commits?page=1&per_page=20';
