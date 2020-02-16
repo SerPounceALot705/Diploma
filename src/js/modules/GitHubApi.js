9be1bf4d2942a828d3effec7af8af190bf3bc262
Valerie Dmitryuk <cosmic.pony705@gmail.com>
2020-02-16 19:42:10 +0800

    Создан класс GitHubApi
    Класс для получения данных о последних 20 коммитах со страницы автора

---
+++ src/js/modules/GitHubApi.js
@@ -0,0 +1,19 @@
+import {GITHUB_URL} from '../constants/Constants';
+
+export default class GitHubApi {
+    constructor() {}
+
+    getCommits(){
+        return new Promise((resolve, reject) => {
+            fetch(GITHUB_URL)
+            .then(result => {
+                if (result.ok) {
+                    return result.json();
+                }
+                return Promise.reject(result);
+            })
+            .then(result => { resolve(result) })
+            .catch(error => reject(error))
+        })
+    }
+}
